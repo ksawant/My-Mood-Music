098a693e158e8eee367d5493d91cd6b17728f734
Kartik <ksawant@purdue.edu>
2014-10-25 07:55:27 -0400

    New user stories added

---
+++ My Mood Music/newaccountbg.h
@@ -0,0 +1,13 @@
+//
+//  newaccountbg.h
+//  My Mood Music
+//
+//  Created by Kartik Sawant on 10/25/14.
+//  Copyright (c) 2014 Kartik Sawant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface newaccountbg : UIViewController
+
+@end
---
+++ My Mood Music/newaccountbg.m
@@ -0,0 +1,38 @@
+//
+//  newaccountbg.m
+//  My Mood Music
+//
+//  Created by Kartik Sawant on 10/25/14.
+//  Copyright (c) 2014 Kartik Sawant. All rights reserved.
+//
+
+#import "newaccountbg.h"
+
+@interface newaccountbg ()
+
+@end
+
+@implementation newaccountbg
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background.jpg"]];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
