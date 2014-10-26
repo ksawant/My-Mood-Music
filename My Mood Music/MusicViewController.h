c0c98f54b2bff2951ee01964d00aa0e5961e01cc
Kartik <ksawant@purdue.edu>
2014-10-26 11:29:54 -0400

    Speed done. Not tested

---
+++ My Mood Music/MusicViewController.h
@@ -0,0 +1,18 @@
+//
+//  MusicViewController.h
+//  My Mood Music
+//
+//  Created by Kartik Sawant on 10/26/14.
+//  Copyright (c) 2014 Kartik Sawant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface MusicViewController : UIViewController <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CLLocation *location;
+@property (weak, nonatomic) IBOutlet UILabel *speed;
+
+@end
---
+++ My Mood Music/MusicViewController.m
@@ -0,0 +1,49 @@
+//
+//  MusicViewController.m
+//  My Mood Music
+//
+//  Created by Kartik Sawant on 10/26/14.
+//  Copyright (c) 2014 Kartik Sawant. All rights reserved.
+//
+
+#import "MusicViewController.h"
+
+@interface MusicViewController ()
+
+@end
+
+@implementation MusicViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+    [self.locationManager startUpdatingLocation];
+    
+    self.locationManager.delegate = self;
+    self.location = [[CLLocation alloc] init];
+    // Do any additional setup after loading the view.
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    self.location = locations.lastObject;
+    NSLog(@"%@", self.location.description);
+    self.speed.text = [NSString stringWithFormat:@"%f", self.location.speed];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
